963dfefa06c646e4ecb0385c30b5271babdea845
Victor Cruz <vicdan215@gmail.com>
2021-06-06 17:19:54 -0500

    Change

--- convnet.py
+++ convnet.py
@@ -174,6 +174,7 @@ def get_encoder(input_img):
     x = MaxPooling2D((2, 2))(x)
     x = useBlockEncoder(x, 32)
     x = MaxPooling2D((2, 2))(x)
+    x = useBlockEncoder(x, 64)
     x = useBlockEncoder(x, constants.domain)
 
     # Produces an array of size equal to constants.domain.
