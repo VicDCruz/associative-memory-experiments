245a9ea8216fad38d50fb9d4980f1fb0449834c5
Victor Cruz <vicdan215@gmail.com>
2021-06-13 02:02:11 -0500

    Update before flatten to run experiments

--- convnet.py
+++ convnet.py
@@ -173,7 +173,7 @@ def get_encoder(input_img):
 
     x = Conv2D(32, kernel_size=3, activation='relu', padding='same',
                input_shape=(img_columns, img_rows, constants.colors))(input_img)
-    x = useBlockEncoder(x, 32, kernelSize=3)
+    x = useBlockEncoder(x, 32, repeat=2, kernelSize=3)
     x = useBlockEncoder(x, 64, kernelSize=3)
     x = useBlockEncoder(x, 128, repeat=2, kernelSize=3)
     x = useBlockEncoder(x, constants.domain, kernelSize=3, strides=1)
@@ -202,13 +202,13 @@ def get_decoder(encoded):
     hid_decoded = decoder_hid(z)
 
     # dense = Dense(units=2 * 2 * 512, activation='relu', input_shape=(constants.domain, ))(encoded)
-    dense = Dense(units=2 * 2 * 512, activation='relu', input_shape=(constants.domain, ))(hid_decoded)
+    dense = Dense(units=1 * 1 * 512, activation='relu', input_shape=(constants.domain, ))(hid_decoded)
     # dense = Dense(units=4 * 4 * 32, activation='relu')(encoded)
-    reshape = Reshape((2, 2, 512))(dense)
+    reshape = Reshape((1, 1, 512))(dense)
     x = useBlockDecoder(reshape, 256, kernelSize=3)
     x = useBlockDecoder(x, 128, kernelSize=3)
     x = useBlockDecoder(x, 64, kernelSize=3)
-    drop_2 = useBlockDecoder(x, 32, kernelSize=3)
+    drop_2 = useBlockDecoder(x, 32, repeat=2, kernelSize=3)
     # drop_2 = Dropout(0.4)(x)
     output_img = Conv2DTranspose(constants.colors, kernel_size=3, strides=1,
                         activation='sigmoid', padding='same', name='autoencoder')(drop_2)
